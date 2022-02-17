1408f0c6ac2d79078a959e2eaf251d3253a49df8
bayronportilla <bayron232@gmail.com>
2022-02-17 17:13:34 +0100

    generalization of gofish script

--- coda/mcmax/profiles.py
+++ coda/mcmax/profiles.py
@@ -1,52 +1,76 @@
 from coda.mcmax.header import *
 from matplotlib.colors import LogNorm
-''' 
-This library is largely based on the Gofish package by 
-Richard Teague 
+
 '''
 
-def get_profile(image,padir,
-                visual=None,write=None,compara=None,fov=None,
-                bunit=None,pxscale=None):
+This module is largely based on the Gofish package by Richard Teague.
 
+'''
+
+def get_profile(image,dist,pa,inc,aperture,
+                visual=None,write=None,fov=None,bunit=None,
+                pxscale=None):
+
+    """
+
+    Extract the signal from a disk using concentric annuli and a conical
+    aperture with user-defined properties.
+
+    Parameters
+    ----------
+    image       : path to the data cube (str)
+    dist        : distance to the disk [pc] (float)
+    pa          : the position angle of the disk [deg] (float)
+    inc         : inclination of the disk [deg] (float)
+    aperture    : defines orientation, width, and radial stepsize of the
+                aperture [deg,deg,arcsec] (list). This quantities are defined
+                on the disk frame. For example, orientation=+10 deg means that
+                the axis of symetry of the aperture is 10 deg apart from the
+                disk's major axis (defined by the position angle argument)
+
+    If you want to create an azimuthally averaged profile just use:
+    >>> aperture=[0,360,stepsize]
+
+    Output
+    ------
+    A three-column plain text file containing the distance from the star in
+    arcsec, and the average intensity and standard deviation in the units
+    specified by the input data cube.
+
+    """
+
+    # Load the data cube
     cube=imagecube(image,FOV=fov,bunit=bunit,pixel_scale=pxscale)
-    widir=20.0
+
+    # Determine limits of the conical aperture
+    padir=aperture[0]
+    widir=aperture[1]
+    dr=aperture[2]
     PAmin=padir-0.5*widir
     PAmax=padir+0.5*widir
-    xm, ym, dym = cube.radial_profile(inc=51.7,PA=158.6,dist=113.43,
-                                      x0=0.0,y0=0.0,assume_correlated=False,
-                                      PA_min=PAmin,PA_max=PAmax,dr=0.025,
-                                      unit='Jy/beam')
-    xm=xm*113.43
 
+    # Extracting radial profile with GoFish
+    xm, ym, dym = cube.radial_profile(inc=inc,PA=pa,dist=dist,
+                                      x0=0.0,y0=0.0,assume_correlated=False,
+                                      PA_min=PAmin,PA_max=PAmax,dr=dr)
+
+    # Plot the aperture?
     if visual:
         vmax=np.percentile(cube.data,99)
         vmin=np.percentile(cube.data,1)
         fig,(ax1,ax2)=plt.subplots(1,2,figsize=(12,5))
         ax1.imshow(cube.data,origin='lower',extent=cube.extent,vmin=vmin,vmax=vmax)
-        cube.plot_mask(ax=ax1,r_min=0.02,r_max=3.0,PA_min=PAmin,PA_max=PAmax,inc=51.7,PA=158.6,mask_frame='disk')
+        cube.plot_mask(ax=ax1,PA_min=PAmin,PA_max=PAmax,
+                        inc=inc,PA=pa,mask_frame='disk',r_max=1.5)
         ax2.errorbar(xm,ym,dym)
-        ax2.set(xlabel="r (AU)",ylabel="flux density")
+        ax2.set(xlabel="r (arcsec)",ylabel="Intensity/flux density")
         plt.show()
+
+    # Write to file?
     if write:
-        file=open("extracted_signal.dat","w")
+        file=open(image+".radial","w")
         for i,j,k in zip(xm,ym,dym):
             file.write("%.15e %.15e %.15e\n"%(i,j,k))
         file.close()
-    if compara:
-        data_keppler=np.loadtxt("../cut_along_c_obs.dat")
-        x=np.reshape(data_keppler[:,0:1],data_keppler.shape[0])
-        y=np.reshape(data_keppler[:,1:2],data_keppler.shape[0])
-        dy=np.reshape(data_keppler[:,2:3],data_keppler.shape[0])
-        
-        fig,ax=plt.subplots()
-        ax.errorbar(x,y,dy,label="keppler")
-        ax.errorbar(x,y*0.75,dy*0.75,label="keppler x 0.75")
-        ax.errorbar(xm,ym*1000,dym*1000,label="isella")
-        ax.set(xlabel="r (AU)",ylabel="flux density (mJy/beam)")
-        ax.legend()
-        plt.show()
-        
-    return None
 
-#get_profile(visual=True,write=False,compara=False)
+    return None
