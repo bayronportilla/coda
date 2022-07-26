64a3a7124d0a436f08c674faf6deb5861b7e8c01
bayronportilla <bayron232@gmail.com>
2022-07-26 14:46:31 +0200

    Plotting surface of emission while extracting radial profiles

--- coda/mcmax/profiles.py
+++ coda/mcmax/profiles.py
@@ -61,16 +61,30 @@ def get_profile(image,dist,pa,inc,aperture,
 
         # Plot the aperture?
         if visual:
+
             vmax=np.percentile(cube.data,99)
             vmin=np.percentile(cube.data,1)
-            fig,(ax1,ax2)=plt.subplots(1,2,figsize=(12,5))
+
+            fig,((ax1,ax2),(ax3,ax4))=plt.subplots(2,2,figsize=(8,8))
+
+            # Image with mask
             ax1.imshow(cube.data,origin='lower',extent=cube.extent,vmin=vmin,vmax=vmax)
             cube.plot_mask(ax=ax1,PA_min=PAmin,PA_max=PAmax,
                             inc=inc,PA=pa,mask_frame='disk',r_max=1.5)
+
+            # Emitting surface
+            cube.plot_surface(ax=ax2)
+
+            # Image + surface
+            ax3.imshow(cube.data,origin='lower',extent=cube.extent,vmin=vmin,vmax=vmax)
+            cube.plot_surface(ax=ax3)
+
+
+            # Radial profile
             if ebar is True:
-                ax2.errorbar(xm,ym,dym)
+                ax4.errorbar(xm,ym,dym)
             if ebar is None:
-                ax2.plot(xm,ym)
+                ax4.plot(xm,ym)
             ax2.set(xlabel="r (arcsec)",ylabel="Intensity/flux density")
             plt.show()
 
@@ -84,7 +98,9 @@ def get_profile(image,dist,pa,inc,aperture,
     else:
 
         print("\n Working with a data cube \n")
-
+        print(inc)
+        print(pa)
+        print(dist)
         # Determine limits of the conical aperture
         padir=aperture[0]
         widir=aperture[1]
@@ -93,27 +109,44 @@ def get_profile(image,dist,pa,inc,aperture,
         PAmax=padir+0.5*widir
 
         # Extracting radial profile with GoFish
-
+        """
         xm, ym, dym = cube.radial_profile(inc=inc,PA=pa,dist=dist,
                                           x0=0.0,y0=0.0,assume_correlated=False,
                                           PA_min=PAmin,PA_max=PAmax,dr=dr,
                                           unit=unit)
         """
-        xm, ym, dym = cube.radial_profile(inc=inc,PA=pa,dist=dist,
-                                          unit=unit)
-        """
 
+        """
+        xm, ym, dym = cube.radial_profile(inc=inc,PA=pa,dist=dist,mstar=0.76,
+                                          unit=unit)
+
+        plt.plot(xm,ym,'.')
+        plt.show()
+
+
+        f=open("Tpeak_observation.dat","w")
+
+
+        for i,j in zip(xm,ym):
+            f.write("%.15e  %.15e\n"%(i,j))
+        f.close()
+        plt.plot(xm,ym,'.')
+        plt.show()
+        """
+        """
         file=open("13CO.radial.cube.WithAperture","w")
         for i,j,k in zip(xm,ym,dym):
             file.write("%.15e %.15e %.15e\n"%(i,j,k))
         file.close()
+        """
+
+        # Plotting teardrop plot
+        cube.plot_teardrop(inc=inc, PA=pa,mstar=0.76, dist=dist)
+        #cube.plot_teardrop(inc=51.7, PA=160.4,mstar=1.0, dist=dist)
+
+        plt.show()
 
         #plt.plot(xm,ym,'.')
         #plt.show()
-
-
-
-
-
 
     return None
