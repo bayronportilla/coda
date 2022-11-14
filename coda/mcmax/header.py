854fb201d68a1ce2c89462cc1736049c174f8b11
bayronportilla <bayron232@gmail.com>
2022-11-14 10:31:19 +0100

    add scipy.stats.norm to header. Needed to compute gaussian PDF

--- coda/mcmax/header.py
+++ coda/mcmax/header.py
@@ -14,8 +14,9 @@ from prodimopy.read import read_prodimo
 from scipy.interpolate import CubicSpline
 from scipy.interpolate import interp2d
 from scipy.interpolate import Rbf
+from scipy.stats import norm
 import pandas as pd
 from gofish import imagecube
-from astropy.convolution import convolve,Gaussian2DKernel   
+from astropy.convolution import convolve,Gaussian2DKernel
 from matplotlib.colors import LogNorm
 from astropy.modeling import models
