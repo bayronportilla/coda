6483311a914a49766e8a6ecc18147dcda7d6d15c
bayronportilla <bayron232@gmail.com>
2023-04-21 18:00:47 +0200

    create radmc3d directory

---
+++ coda/radmc3d/input.py
@@ -0,0 +1,15 @@
+from coda.mcmax.header import *
+
+def wavelength_grid(lam_min,lam_max):
+    '''
+
+    Creates a one column file with the value of discrete wavelength points where
+    continuum radiative transfer will be performed.
+
+    
+
+    '''
+
+
+
+    return None
