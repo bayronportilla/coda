f83f5d0ce381b04373d2044af3b5db82d3d7cf1a
bayronportilla <bayron232@gmail.com>
2023-04-24 18:44:47 +0200

    a very basic wavelength_grid() method

--- coda/radmc3d/input.py
+++ coda/radmc3d/input.py
@@ -1,15 +1,23 @@
 from coda.mcmax.header import *
 
 def wavelength_grid(lam_min,lam_max):
+
     '''
 
     Creates a one column file with the value of discrete wavelength points where
     continuum radiative transfer will be performed.
 
-    
-
     '''
 
+    Nlam        = 150
+    wl_array    = np.logspace(np.log10(lam_min),np.log10(lam_max),Nlam,)
 
+    file = open("wavelength_micron.inp","w")
+    file.write("%d\n"%Nlam)
+    for i in wl_array:
+        print(i)
+        file.write(" %.5e \n"%i)
 
     return None
+
+def stars()
